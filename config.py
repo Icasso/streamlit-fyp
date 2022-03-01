263495929da77b98ffc1d439f69b258aa4f9ee8d
Icasso <isaactsui2000@gmail.com>
2022-03-01 21:17:15 +0800

    03/01/22: Update

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-DB_HOST = 'localhost'
-DB_USER = 'postgres'
-DB_PASS = ''
-DB_NAME = 'Test'
-# DB_HOST = 'fyp-psql.postgres.database.azure.com'
-# DB_USER = 'tsuihoiming'
-# DB_PASS = 'Qw645987321'
-# DB_NAME = 'Development'
+# DB_HOST = 'localhost'
+# DB_USER = 'postgres'
+# DB_PASS = ''
+# DB_NAME = 'Test'
+import streamlit as st
+DB_HOST = st.secrets["DB_HOST"]
+DB_USER = st.secrets["DB_USER"]
+DB_PASS = st.secrets["DB_PASS"]
+DB_NAME = st.secrets["DB_NAME"]
